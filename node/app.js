4a2c4bf5090df99843dcd07b9ae6b0778bd455d8
ikgoo <lhscj2466@gmail.com>
2016-12-02 09:32:18 +0900

    init

--- node/app.js
+++ node/app.js
@@ -1,1 +1,17 @@
-console.log("test");
+var express = require('express')
+    , http = require('http')
+    , app = express()
+    , server = http.createServer(app);
+
+app.get('/', function(req, res) {
+    res.send('Hello /');
+});  
+
+app.get('/world.html', function (req, res) {
+    res.send('Hello World');
+});
+
+server.listen(8001, function() {
+    console.log('Express server listening on port ' + server.address().port);
+});
+
